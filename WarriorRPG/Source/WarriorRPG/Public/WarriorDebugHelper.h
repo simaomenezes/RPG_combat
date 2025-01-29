14e7f4913bd227646a001158993ec0957dc2fc44
Simão Neto <simaomenezesneto@gmail.com>
2025-01-29 20:15:53 +0000

    [Section-2: Set Up Hero Character] feat: Make Native Gameplay Tags in c++

--- WarriorRPG/Source/WarriorRPG/Public/WarriorDebugHelper.h
+++ WarriorRPG/Source/WarriorRPG/Public/WarriorDebugHelper.h
@@ -2,11 +2,11 @@
 
 namespace Debug
 {
-	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 Inkey = -1)
+	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 InKey = -1)
 	{
 		if (GEngine)
 		{
-			GEngine->AddOnScreenDebugMessage(-1, Inkey, Color, Msg);
+			GEngine->AddOnScreenDebugMessage(InKey,7.f,Color,Msg);
 
 			UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
 		}
